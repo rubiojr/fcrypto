3bbea74dad68dcb74032bbc5789ada4b137ec16c
Sergio Rubio <rubiojr@frameos.org>
2019-06-25 22:25:34 +0200

    Make the example simpler

--- examples/basic/basic.go
+++ examples/basic/basic.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 
@@ -11,12 +12,10 @@ const secret = "bar"
 const file = "secret.conf"
 
 func main() {
-	needsSave := false
 
 	if _, err := os.Stat(file); os.IsNotExist(err) {
-		fmt.Println("Creating an empty file and adding 'foobar' to it")
-		needsSave = true
-		fcrypto.CreateFile(file, secret)
+		fmt.Println("Saving a file with 'foobar'")
+		fcrypto.SaveFile(bytes.NewBufferString("foobar"), file, secret)
 	} else {
 		fmt.Println("Loading existing " + file)
 	}
@@ -25,15 +24,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	if needsSave {
-		buf.Write([]byte("foobar"))
-		fmt.Printf("Encrypting the file %s with password '%s'\n", file, secret)
-		err := fcrypto.SaveFile(buf, file, secret)
-		if err != nil {
-			fmt.Println(err)
-		}
-	} else {
-		fmt.Println(buf.String())
-	}
+	fmt.Println("Decrypting the file...")
+	fmt.Println("File content: " + buf.String())
 }
